b3863ff2fdad804cce2c7234227467857f2dd44d
Viktor Danek <viktordanek10@gmail.com>
2025-03-02 09:52:48 -0500

    MUCH BETTER

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
                                     string ? null
                                 } @simple :
                                     {
-                                        default ? path : value : builtins.throw "The definition at ${ builtins.concatStringsSep " / " ( builtins.map builtins.toJSON path ) } is invalid.  It is of type ${ builtins.typeOf value }.  It is ${ if builtins.any ( t : t == builtins.typeOf value ) [ "bool" "float" "int" "null" "path" "string" ] then  builtins.toJSON value else "unstringable." }." ,
+                                        default ? path : value : builtins.throw "The definition at ${ builtins.concatStringsSep " / " ( builtins.concatLists [ [ "*ROOT*" ] ( builtins.map builtins.toJSON path ) ] ) } is invalid.  It is of type ${ builtins.typeOf value }.  It is ${ if builtins.any ( t : t == builtins.typeOf value ) [ "bool" "float" "int" "null" "path" "string" ] then  builtins.toJSON value else "unstringable." }." ,
                                         list ? list : list ,
                                         set ? set : set
                                     } : value :
