aace1b45a2e5dea24cc2c32f60da50f18d0054b6
Viktor Danek <viktordanek10@gmail.com>
2025-03-02 09:15:29 -0500

    infinite recursion

--- flake.nix
+++ flake.nix
@@ -37,13 +37,13 @@
                                                                             let
                                                                                 all-visitors =
                                                                                     let
-                                                                                        list-visitor =
+                                                                                        list-visitor = builtins.trace "bec736f2-4b99-4fc1-a471-fc4472983fa2" (
                                                                                             if builtins.typeOf list == "lambda" then
                                                                                                 {
                                                                                                     name = "list" ;
                                                                                                     value = path : value : list ( builtins.genList ( index : elem ( builtins.concatLists [ path [ index ] ] ) value ) ( builtins.length value ) ) ;
                                                                                                 }
-                                                                                            else builtins.throw "The complex list aggregator is not lambda but ${ builtins.typeOf list }." ;
+                                                                                            else builtins.throw "The complex list aggregator is not lambda but ${ builtins.typeOf list }." ) ;
                                                                                         simple-visitors =
                                                                                             let
                                                                                                 simple-visitors =
@@ -91,7 +91,7 @@
                                                                         in builtins.head filtered-visitors ;
                                                                 in visitor.value ;
                                                         in visitor path value ;
-                                            in elem [ ] value ;
+                                            in builtins.trace "496f8947-31af-46d2-9e4a-29e40cb2f602 ${ builtins.typeOf value }" ( elem [ ] value ) ;
                             pkgs = builtins.import nixpkgs { system = system ; } ;
                             in
                                 {
@@ -111,7 +111,7 @@
                                                                                     success = if builtins.typeOf expected-success == "bool" then expected-success else builtins.throw "The expected success of ${ name } should be a boolean."  ;
                                                                                     value = if builtins.any ( t : t == builtins.typeOf expected-value ) [ "bool" "float" "int" "null" "string" "path" ] then expected-value else builtins.throw "The expected value of ${ name } is not stringable but ${ builtins.typeOf expected-value }." ;
                                                                                 } ;
-                                                                            observed = builtins.tryEval ( observation ( lib simple complex visited ) ) ;
+                                                                            observed = builtins.tryEval ( observation ( builtins.trace "4e215efa-2aed-4dbc-b8cf-0ced4abe5f5d" ( ( builtins.trace "d963b61b-8bbe-4e78-8b50-164d08768f79" lib ) ( builtins.trace "44439799-bdbf-411e-8507-fe2fb0a434ee" simple ) ( builtins.trace "2353bffa-0403-4e14-ae44-022139135583 ${ builtins.typeOf complex }" complex ) ( builtins.trace "608947e3-3140-4571-8999-a8f6bba1650d" visited ) ) ) ) ;
                                                                             in
                                                                                 if expected == observed then
                                                                                     ''
@@ -133,9 +133,9 @@
                                             in
                                                 builtins.listToAttrs
                                                     [
-                                                        ( check "set" { string = path : value : value ; } { } { alpha = "512f3471c79f2cb9f99ec4ebe152158bb114189d2f5882541442fc5d539da43901a29b85d915253ee3d58d636a364804772410af112a6a6c99f54d2a56bfedb2" ; } ( candidate : candidate.alpha ) true "512f3471c79f2cb9f99ec4ebe152158bb114189d2f5882541442fc5d539da43901a29b85d915253ee3d58d636a364804772410af112a6a6c99f54d2a56bfedb2" )
-                                                        ( check "string" { string = path : value : value ; } { } "9a9115b8c7fe5ec423464e181946afaa6639b8f2792afee8f8dd76d07607c476c234918fbdd6f2a254098ec30958bae2414b0a39b72ca69cdbfcbf8c310d830f" ( candidate : candidate ) true "9a9115b8c7fe5ec423464e181946afaa6639b8f2792afee8f8dd76d07607c476c234918fbdd6f2a254098ec30958bae2414b0a39b72ca69cdbfcbf8c310d830f" )
-                                                        # ( check "list" { string = path : value : value ; } [ ] { alpha = "70c5852246c99b7c36cac790cf1627f3d89e6c6d55214c734e500292ab9007c013b0a2f04382f92791568f328f11fe4bb7bb99718be2b86ca3608148b3da9e06" ; } ( candidate : builtins.elemAt candidate 0 ) true "70c5852246c99b7c36cac790cf1627f3d89e6c6d55214c734e500292ab9007c013b0a2f04382f92791568f328f11fe4bb7bb99718be2b86ca3608148b3da9e06" )
+                                                        # ( check "set" { string = path : value : value ; } { } { alpha = "512f3471c79f2cb9f99ec4ebe152158bb114189d2f5882541442fc5d539da43901a29b85d915253ee3d58d636a364804772410af112a6a6c99f54d2a56bfedb2" ; } ( candidate : candidate.alpha ) true "512f3471c79f2cb9f99ec4ebe152158bb114189d2f5882541442fc5d539da43901a29b85d915253ee3d58d636a364804772410af112a6a6c99f54d2a56bfedb2" )
+                                                        # ( check "string" { string = path : value : value ; } { } "9a9115b8c7fe5ec423464e181946afaa6639b8f2792afee8f8dd76d07607c476c234918fbdd6f2a254098ec30958bae2414b0a39b72ca69cdbfcbf8c310d830f" ( candidate : candidate ) true "9a9115b8c7fe5ec423464e181946afaa6639b8f2792afee8f8dd76d07607c476c234918fbdd6f2a254098ec30958bae2414b0a39b72ca69cdbfcbf8c310d830f" )
+                                                        ( check "list" { string = path : value : value ; } { } [ "9a9115b8c7fe5ec423464e181946afaa6639b8f2792afee8f8dd76d07607c476c234918fbdd6f2a254098ec30958bae2414b0a39b72ca69cdbfcbf8c310d830f" ] ( candidate : builtins.elemAt candidate 0 ) true "9a9115b8c7fe5ec423464e181946afaa6639b8f2792afee8f8dd76d07607c476c234918fbdd6f2a254098ec30958bae2414b0a39b72ca69cdbfcbf8c310d830f" )
                                                     ] ;
                                     lib = lib ;
                                 } ;
