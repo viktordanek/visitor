51530086f95e8460617605f1319d1e103de3cf6f
Viktor Danek <viktordanek10@gmail.com>
2025-03-02 06:51:48 -0500

    WTF

--- flake.nix
+++ flake.nix
@@ -24,96 +24,78 @@
                                         default ? path : value : "The definition at ${ builtins.concatStringsSep " / " ( builtins.map builtins.toJSON path ) } is invalid.  It is of type ${ builtins.typeOf value }.  It is ${ if builtins.type.any ( t : t == builtins.typeOf value ) [ "bool" "float" "int" "path" "string" ] then  builtins.toJSON value else "unstringable." }." ,
                                         list ? list : list ,
                                         set ? set : set
-                                    } @complex : default : value :
-                                    let
-                                        elem =
-                                            path : value :
-                                                let
-                                                    visitor =
-                                                        let
-                                                            list-visitor = path : value : list ( builtins.genList ( index : elem ( builtins.concatLists [ path [ index ] ] ) value ) ( builtins.length value ) ) ;
-                                                            simple-visitors =
-                                                                let
-                                                                    mapper =
-                                                                        name : value :
-                                                                            {
-                                                                                name = name ;
-                                                                                value =
-                                                                                    if builtins.typeOf value == "lambda" then value
-                                                                                    else if builtins.typeOf value == "null" then default
-                                                                                    else builtins.throw "The ${ name } visitor is not lambda, null but ${ builtins.typeOf value }." ;
-                                                                            } ;
-                                                                    in builtins.mapAttrs mapper simple ;
-                                                            set-visitor = path : value : set ( builtins.mapAttrs ( name : value : elem ( builtins.concatList [ path [ name ] ] ) value ) value ) ;
-                                                            in builtins.listToAttrs ( builtins.concatLists [ simple-visitors [ list-visitor set-visitor ] ] ) ;
-                                                    in visitor path value ;
-                                        in elem [ ] value ;
+                                    } : value :
+                                        let
+                                            elem =
+                                                path : value :
+                                                    let
+                                                        visitor =
+                                                            let
+                                                                list-visitor =
+                                                                    if builtins.typeOf list == "lambda" then path : value : list ( builtins.genList ( index : elem ( builtins.concatLists [ path [ index ] ] ) value ) ( builtins.length value ) )
+                                                                    else builtins.throw "The complex list aggregator is not lambda but ${ builtins.typeOf list }." ;
+                                                                simple-visitors =
+                                                                    let
+                                                                        mapper =
+                                                                            name : value :
+                                                                                {
+                                                                                    name = name ;
+                                                                                    value =
+                                                                                        if builtins.typeOf value == "lambda" then value
+                                                                                        else if builtins.typeOf value == "null" && builtins.typeOf default == "lambda" then default
+                                                                                        else if builtins.typeOf value == "null" then builtins.throw "The simple ${ name } visitor is not lambda, null (and the default simple visitor is not lambda) but ${ builtins.typeOf value }."
+                                                                                        else builtins.throw "The simple ${ name } visitor is not lambda, null but ${ builtins.typeOf value }." ;
+                                                                                } ;
+                                                                        in builtins.mapAttrs mapper simple ;
+                                                                set-visitor =
+                                                                    if builtins.typeOf set == "lambda" then path : value : set ( builtins.mapAttrs ( name : value : elem ( builtins.concatList [ path [ name ] ] ) value ) value )
+                                                                    else builtins.throw "The complex set aggregator is not lambda but ${ builtins.typeOf set }." ;
+                                                                in builtins.listToAttrs ( builtins.concatLists [ simple-visitors [ list-visitor set-visitor ] ] ) ;
+                                                        in visitor path value ;
+                                            in elem [ ] ;
                             pkgs = builtins.import nixpkgs { system = system ; } ;
                             in
                                 {
                                     checks =
-                                        {
-                                            defaults = pkgs.stdenv.mkDerivation
+                                        let
+                                            check =
+                                                name : simple : complex : visited : observation : success : value :
                                                     {
-                                                        installPhase =
-                                                            let
-                                                                candidate = lib { } { } ;
-                                                                expected = { success = false ; value = false ; } ;
-                                                                observed = builtins.tryEval ( candidate [ ] ) ;
-                                                                in
-                                                                    if expected == observed then
-                                                                        ''
-                                                                            ${ pkgs.coreutils }/bin/touch $out
-                                                                        ''
-                                                                     else
-                                                                        ''
-                                                                            ${ pkgs.coreutils }/bin/touch $out &&
-                                                                                ${ pkgs.coreutils }/bin/echo EXPECTED:  ${ builtins.toJSON expected } &&
-                                                                                ${ pkgs.coreutils }/bin/echo OBSERVED:  ${ builtins.toJSON observed } &&
-                                                                                exit 64
-                                                                        '' ;
-                                                        name = "visitor-checks" ;
-                                                        src = ./. ;
+                                                        name = name ;
+                                                        value =
+                                                            pkgs.stdenv.mkDerivation
+                                                                {
+                                                                    installPhase =
+                                                                        let
+                                                                            expected =
+                                                                                {
+                                                                                    success = success ;
+                                                                                    value = value ;
+                                                                                } ;
+                                                                            observed = builtins.tryEval ( observation ( lib simple complex visited ) ) ;
+                                                                            in
+                                                                                if expected == observed then
+                                                                                    ''
+                                                                                        ${ pkgs.coreutils }/bin/touch $out
+                                                                                    ''
+                                                                                else
+                                                                                    ''
+                                                                                        ${ pkgs.coreutils }/bin/touch $out &&
+                                                                                            ${ pkgs.coreutils }/bin/echo CHECK:  ${ name } >&2 &&
+                                                                                            ${ pkgs.coreutils }/bin/echo EXPECTED did not equal OBSERVED >&2 &&
+                                                                                            ${ pkgs.coreutils }/bin/echo EXPECTED:  ${ builtins.toJSON expected } >&2 &&
+                                                                                            ${ pkgs.coreutils }/bin/echo OBSERVED:  ${ builtins.toJSON observed } >&2 &&
+                                                                                            exit 64
+                                                                                    '' ;
+                                                                    name = "visitor-check-${ name }" ;
+                                                                    src = ./. ;
+                                                                } ;
                                                     } ;
-                                            lazy = pkgs.stdenv.mkDerivation
-                                                    {
-                                                        installPhase =
-                                                            let
-                                                                candidate =
-                                                                    lib
-                                                                        {
-                                                                            lambda = path : visitor : "lambda" ;
-                                                                            null = path : visitor : "null" ;
-                                                                        } { } ;
-                                                                expected = { complex = { lambda = "lambda" ; null = "null" ; } ; } ;
-                                                                observed = builtins.tryEval ( builtins.getAttr "complex" ( builtins.getAttr "lambda" ( candidate { complex = { bool = false ; lambda = x : x ; null = null ; } ; } ) ) ) ;
-                                                                in
-                                                                    ''
-                                                                        ${ pkgs.coreutils }/bin/touch $out
-                                                                    '' ;
-                                                        name = "visitor-checks" ;
-                                                        src = ./. ;
-                                                    } ;
-                                            standard = pkgs.stdenv.mkDerivation
-                                                    {
-                                                        installPhase =
-                                                            let
-                                                                candidate =
-                                                                    lib
-                                                                        {
-                                                                            lambda = path : visitor : "lambda" ;
-                                                                            null = path : visitor : "null" ;
-                                                                        } { } ;
-                                                                expected = { complex = { lambda = "lambda" ; null = "null" ; } ; } ;
-                                                                observed = builtins.tryEval ( candidate { complex = { lambda = x : x ; null = null ; } ; } ) ;
-                                                                in
-                                                                    ''
-                                                                        ${ pkgs.coreutils }/bin/touch $out
-                                                                    '' ;
-                                                        name = "visitor-checks" ;
-                                                        src = ./. ;
-                                                    } ;
-                                        } ;
+                                            in
+                                                builtins.listToAttrs
+                                                    [
+                                                        ( check "easy" { string = value : value ; } { } { alpha = "a" ; } ( candidate : builtins.typeOf candidate ) false false )
+                                                    ] ;
                                     lib = lib ;
                                 } ;
                 in flake-utils.lib.eachDefaultSystem fun ;
