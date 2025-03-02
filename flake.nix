caf6568108fc096776f8dacfa452b58696767b02
Viktor Danek <viktordanek10@gmail.com>
2025-03-02 09:50:12 -0500

    better

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
                                     string ? null
                                 } @simple :
                                     {
-                                        default ? path : value : "The definition at ${ builtins.concatStringsSep " / " ( builtins.map builtins.toJSON path ) } is invalid.  It is of type ${ builtins.typeOf value }.  It is ${ if builtins.type.any ( t : t == builtins.typeOf value ) [ "bool" "float" "int" "path" "string" ] then  builtins.toJSON value else "unstringable." }." ,
+                                        default ? path : value : "The definition at ${ builtins.concatStringsSep " / " ( builtins.map builtins.toJSON path ) } is invalid.  It is of type ${ builtins.typeOf value }.  It is ${ if builtins.any ( t : t == builtins.typeOf value ) [ "bool" "float" "int" "null" "path" "string" ] then  builtins.toJSON value else "unstringable." }." ,
                                         list ? list : list ,
                                         set ? set : set
                                     } : value :
@@ -50,14 +50,14 @@
                                                                                                     let
                                                                                                         identity =
                                                                                                             {
-                                                                                                                bool ? null ,
-                                                                                                                float ? null ,
-                                                                                                                int ? null ,
-                                                                                                                lambda ? null ,
-                                                                                                                null ? null ,
-                                                                                                                path ? null ,
-                                                                                                                string ? null
-                                                                                                            } :
+                                                                                                                bool ? builtins.null ,
+                                                                                                                float ? builtins.null ,
+                                                                                                                int ? builtins.null ,
+                                                                                                                lambda ? builtins.null ,
+                                                                                                                null ? builtins.null ,
+                                                                                                                path ? builtins.null ,
+                                                                                                                string ? builtins.null
+                                                                                                            } @p :
                                                                                                                 {
                                                                                                                     bool = bool ;
                                                                                                                     float = float ;
@@ -67,15 +67,15 @@
                                                                                                                     string = string ;
                                                                                                                 } ;
                                                                                                         mapper =
-                                                                                                            name : value :
+                                                                                                            name : value : builtins.trace "5da96c4e-e5c8-4009-805d-882b19433c44:  ${ name } ${ builtins.typeOf default }" (
                                                                                                                 {
                                                                                                                     name = name ;
                                                                                                                     value =
-                                                                                                                        if builtins.typeOf value == "lambda" then value
-                                                                                                                        else if builtins.typeOf value == "null" && builtins.typeOf default == "lambda" then default
+                                                                                                                        if builtins.typeOf value == "lambda" then builtins.trace "6bedc3f4-1cd7-4750-a0bd-4ba37abeeacf" value
+                                                                                                                        else if builtins.typeOf value == "null" && builtins.typeOf default == "lambda" then builtins.trace "995ab945-31ce-44fc-ad9d-a42bf8dcca77" default
                                                                                                                         else if builtins.typeOf value == "null" then builtins.throw "The simple ${ name } visitor is not lambda, null (and the default simple visitor is not lambda) but ${ builtins.typeOf value }."
                                                                                                                         else builtins.throw "The simple ${ name } visitor is not lambda, null but ${ builtins.typeOf value }." ;
-                                                                                                                } ;
+                                                                                                                } ) ;
                                                                                                         in builtins.mapAttrs mapper ( identity ( simple ) ) ;
                                                                                                 in builtins.attrValues simple-visitors ;
                                                                                         set-visitor =
@@ -133,9 +133,10 @@
                                             in
                                                 builtins.listToAttrs
                                                     [
+                                                        ( check "no-visitor" { string = path : value : value ; } { } null ( candidate : candidate ) false false )
                                                         # ( check "set" { string = path : value : value ; } { } { alpha = "512f3471c79f2cb9f99ec4ebe152158bb114189d2f5882541442fc5d539da43901a29b85d915253ee3d58d636a364804772410af112a6a6c99f54d2a56bfedb2" ; } ( candidate : candidate.alpha ) true "512f3471c79f2cb9f99ec4ebe152158bb114189d2f5882541442fc5d539da43901a29b85d915253ee3d58d636a364804772410af112a6a6c99f54d2a56bfedb2" )
                                                         # ( check "string" { string = path : value : value ; } { } "9a9115b8c7fe5ec423464e181946afaa6639b8f2792afee8f8dd76d07607c476c234918fbdd6f2a254098ec30958bae2414b0a39b72ca69cdbfcbf8c310d830f" ( candidate : candidate ) true "9a9115b8c7fe5ec423464e181946afaa6639b8f2792afee8f8dd76d07607c476c234918fbdd6f2a254098ec30958bae2414b0a39b72ca69cdbfcbf8c310d830f" )
-                                                        ( check "list" { string = path : value : value ; } { } [ "c338cd832d312cc4f76bb1a7f9febf96745a9b19a6e5d7cff378f5f4b79fcb0e98d1e4450fcb1f1a87050c45700654f34f878c0a65f9559ef289f3e10e29b700" ] ( candidate : builtins.elemAt candidate 0 ) true "c338cd832d312cc4f76bb1a7f9febf96745a9b19a6e5d7cff378f5f4b79fcb0e98d1e4450fcb1f1a87050c45700654f34f878c0a65f9559ef289f3e10e29b700" )
+                                                        # ( check "list" { string = path : value : value ; } { } [ "c338cd832d312cc4f76bb1a7f9febf96745a9b19a6e5d7cff378f5f4b79fcb0e98d1e4450fcb1f1a87050c45700654f34f878c0a65f9559ef289f3e10e29b700" ] ( candidate : builtins.elemAt candidate 0 ) true "c338cd832d312cc4f76bb1a7f9febf96745a9b19a6e5d7cff378f5f4b79fcb0e98d1e4450fcb1f1a87050c45700654f34f878c0a65f9559ef289f3e10e29b700" )
                                                     ] ;
                                     lib = lib ;
                                 } ;
